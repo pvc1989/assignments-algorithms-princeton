2ed9c3fac40e8e8dd190ae82320de6e408e02103
Vladimir Constantinovich Petrov <weicheng.pei@icloud.com>
2017-02-21 15:30:09 -0600

    define API’s for PointSET.java and KdTree.java

---
+++ kdtree/KdTree.java
@@ -0,0 +1,55 @@
+/******************************************************************************
+ *  Compilation:  javac-algs4 KdTree.java
+ *  Execution:    java-algs4 KdTree
+ *  Dependencies: 
+ *
+ *  a mutable data type that uses a 2d-tree to implement the same API. 
+ *  A 2d-tree is a generalization of a BST to two-dimensional keys. The idea is 
+ *  to build a BST with points in the nodes, using the x- and y-coordinates of 
+ *  the points as keys in strictly alternating sequence.
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdOut;
+
+public class KdTree {
+    
+        // construct an empty set of points
+    public KdTree() {
+        
+    }
+        // is the set empty?
+    public boolean isEmpty() {
+        
+    }
+        // number of points in the set
+    public int size() {
+        
+    }
+        // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        
+    }
+        // does the set contain point p?
+    public boolean contains(Point2D p) {
+        
+    }
+        // draw all points to standard draw
+    public void draw() {
+        
+    }
+        // all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        
+    }
+        // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        
+    }
+        // unit testing of the methods (optional)
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ kdtree/PointSET.java
@@ -0,0 +1,61 @@
+/******************************************************************************
+ *  Compilation:  javac-algs4 PointSET.java
+ *  Execution:    javac-algs4 PointSET
+ *  Dependencies: 
+ *
+ *  a mutable data type that represents a set of points in the unit square using
+ *  a red-black BST.
+ *
+ *  Corner cases:
+ *      Throw a java.lang.NullPointerException if any argument is null.
+ *
+ *  Performance requirements:
+ *      Your implementation should support insert() and contains() in time 
+ *      proportional to the logarithm of the number of points in the set in the 
+ *      worst case; it should support nearest() and range() in time proportional
+ *      to the number of points in the set.
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdOut;
+
+public class PointSET {
+    
+        // construct an empty set of points
+    public PointSET() {
+        
+    }
+        // is the set empty?
+    public boolean isEmpty() {
+        
+    }
+        // number of points in the set
+    public int size() {
+        
+    }
+        // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        
+    }
+        // does the set contain point p?
+    public boolean contains(Point2D p) {
+        
+    }
+        // draw all points to standard draw
+    public void draw() {
+        
+    }
+        // all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        
+    }
+        // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        
+    }
+        // unit testing of the methods (optional)
+    public static void main(String[] args) {
+        
+    }
+}
