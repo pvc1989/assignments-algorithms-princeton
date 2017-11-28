625f30d8b7bca6c83369d9760f5d75a4854afe6b
Weicheng Pei <weicheng.pei@icloud.com>
2017-11-28 20:44:22 +0800

    implement length() and ancestor() for multiple sources

--- wordnet/SAP.java
+++ wordnet/SAP.java
@@ -1,28 +1,22 @@
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Queue;
-import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import java.util.HashMap;
+import java.util.TreeSet;
 
 public class SAP {
 
     private final Digraph g_;
-    private class Node {
-        private final int parent_;
-        private final int depth_;
-        public Node(int parent, int depth) {
-            parent_ = parent;
-            depth_ = depth;
-        }
-        public int Parent() { return parent_; }
-        public int Depth() { return depth_; }
-    }
-    // breadth-first trees rooted at v and w, built in the last query
-    private HashMap<Integer, Node> v_tree_, w_tree_;
-    private int v_, w_;  // vertices specified in the last query
-    private int ca_;  // common ancestor found in the last query
+    // cache for single source query
+    private int v_, w_;
+    private HashMap<Integer, Integer> dist_to_v_, dist_to_w_;
+    private int ca_;  // common ancestor of 2 vertices 
+    // cache for multiple sources query
+    private TreeSet<Integer> v_set_, w_set_;
+    private HashMap<Integer, Integer> dist_to_v_set_, dist_to_w_set_;
+    private int ca_of_2_sets_;  // common ancestor of 2 sets of vertices
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph g) {
@@ -34,21 +28,21 @@ public class SAP {
 
     // Run BFS on both sources, one after another, level by level,
     // to find the common ancestor with minimum path length.
-    private void find_common_ancestor_(int v, int w) {
+    private void bfs_(int v, int w) {
         if (v == v_ && w_ == w) {
             StdOut.print("(Same as the last query.) ");
             return;
         }
 
         v_ = v;
-        v_tree_ = new HashMap<Integer, Node>();
-        v_tree_.put(v, new Node(v, 0));
+        dist_to_v_ = new HashMap<Integer, Integer>();
+        dist_to_v_.put(v, 0);
         Queue<Integer> v_queue = new Queue<Integer>();
         v_queue.enqueue(v);
 
         w_ = w;
-        w_tree_ = new HashMap<Integer, Node>();        
-        w_tree_.put(w, new Node(w, 0));
+        dist_to_w_ = new HashMap<Integer, Integer>();        
+        dist_to_w_.put(w, 0);
         Queue<Integer> w_queue = new Queue<Integer>();
         w_queue.enqueue(w);
 
@@ -57,14 +51,14 @@ public class SAP {
             // Run BFS on v
             if (!v_queue.isEmpty()) {
                 int parent = v_queue.dequeue();
-                if (w_tree_.containsKey(parent)) {
+                if (dist_to_w_.containsKey(parent)) {
                     ca_ = parent;
                     return;
                 }
-                int depth = v_tree_.get(parent).Depth() + 1;
+                int depth = dist_to_v_.get(parent) + 1;
                 for (int kid : g_.adj(parent)) {
-                    if (!v_tree_.containsKey(kid)) {
-                        v_tree_.put(kid, new Node(parent, depth));
+                    if (!dist_to_v_.containsKey(kid)) {
+                        dist_to_v_.put(kid, depth);
                         v_queue.enqueue(kid);
                     }
                 }
@@ -72,14 +66,14 @@ public class SAP {
             // Run BFS on w
             if (!w_queue.isEmpty()) {
                 int parent = w_queue.dequeue();
-                if (v_tree_.containsKey(parent)) {
+                if (dist_to_v_.containsKey(parent)) {
                     ca_ = parent;
                     return;
                 }
-                int depth = w_tree_.get(parent).Depth() + 1;
+                int depth = dist_to_w_.get(parent) + 1;
                 for (int kid : g_.adj(parent)) {
-                    if (!w_tree_.containsKey(kid)) {
-                        w_tree_.put(kid, new Node(parent, depth));
+                    if (!dist_to_w_.containsKey(kid)) {
+                        dist_to_w_.put(kid, depth);
                         w_queue.enqueue(kid);
                     }
                 }
@@ -89,27 +83,93 @@ public class SAP {
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        find_common_ancestor_(v, w);
-        return v_tree_.get(ca_).Depth() + w_tree_.get(ca_).Depth();
+        bfs_(v, w);
+        return dist_to_v_.get(ca_) + dist_to_w_.get(ca_);
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral 
     // path; -1 if no such path
     public int ancestor(int v, int w) {
-        find_common_ancestor_(v, w);
+        bfs_(v, w);
         return ca_;
+    }
+
+    // Run BFS on both sources, one after another, level by level,
+    // to find the common ancestor with minimum path length.
+    private void bfs_(Iterable<Integer> v, Iterable<Integer> w) {
+        TreeSet<Integer> v_set = new TreeSet<Integer>();
+        for (int x : v) v_set.add(x);
+        TreeSet<Integer> w_set = new TreeSet<Integer>();
+        for (int x : w) w_set.add(x);
+        if (v_set.equals(v_set_) && w_set.equals(w_set_)) {
+            StdOut.print("(Same as the last query.) ");
+            return;
+        }
+
+        v_set_ = v_set;
+        dist_to_v_set_ = new HashMap<Integer, Integer>();
+        Queue<Integer> v_queue = new Queue<Integer>();
+        for (int x : v_set) {
+            dist_to_v_set_.put(x, 0);
+            v_queue.enqueue(x);
+        }
+
+        w_set_ = w_set;
+        dist_to_w_set_ = new HashMap<Integer, Integer>();  
+        Queue<Integer> w_queue = new Queue<Integer>();  
+        for (int x : w_set) {
+            dist_to_w_set_.put(x, 0);
+            w_queue.enqueue(x);
+        }
+
+        while (true) {
+            // TODO: merge similar blocks
+            // Run BFS on v_set
+            if (!v_queue.isEmpty()) {
+                int parent = v_queue.dequeue();
+                if (dist_to_w_set_.containsKey(parent)) {
+                    ca_of_2_sets_ = parent;
+                    return;
+                }
+                int depth = dist_to_v_set_.get(parent) + 1;
+                for (int kid : g_.adj(parent)) {
+                    if (!dist_to_v_set_.containsKey(kid)) {
+                        dist_to_v_set_.put(kid, depth);
+                        v_queue.enqueue(kid);
+                    }
+                }
+            }
+            // Run BFS on w_set
+            if (!w_queue.isEmpty()) {
+                int parent = w_queue.dequeue();
+                if (dist_to_v_set_.containsKey(parent)) {
+                    ca_of_2_sets_ = parent;
+                    return;
+                }
+                int depth = dist_to_w_set_.get(parent) + 1;
+                for (int kid : g_.adj(parent)) {
+                    if (!dist_to_w_set_.containsKey(kid)) {
+                        dist_to_w_set_.put(kid, depth);
+                        w_queue.enqueue(kid);
+                    }
+                }
+            }
+        }
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex 
     // in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+        bfs_(v, w);
+        return dist_to_v_set_.get(ca_of_2_sets_) + 
+               dist_to_w_set_.get(ca_of_2_sets_);
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no 
     // such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+        bfs_(v, w);
+        return ca_of_2_sets_;
     }
 
     // do unit testing of this class
@@ -118,19 +178,39 @@ public class SAP {
         SAP sap = new SAP(new Digraph(new In(args[0])));
         StdOut.println("The Digraph:");
         StdOut.println(sap.g_);
-        // test find_common_ancestor_()
+        // test bfs_(int, int)
         StdOut.println("Give 2 query vertices:");        
         int v = StdIn.readInt();
         int w = StdIn.readInt();
         StdOut.printf("Common Ancestor of %d and %d: ", v, w);
-        sap.find_common_ancestor_(v, w);
+        sap.bfs_(v, w);
         StdOut.println(sap.ca_);
-        // test ancestor()
+        // test ancestor(int, int)
         StdOut.printf("Common Ancestor of %d and %d: ", v, w);
         StdOut.println(sap.ancestor(v, w));
-        // test length()
+        // test length(int, int)
         StdOut.printf(
             "Length of shortest ancestral path between %d and %d: ", v, w);
         StdOut.println(sap.length(v, w));
+        // test bfs_(Iterable<Integer>, Iterable<Integer>)
+        StdOut.println("Give 2 query vertices in the 1st set:");        
+        TreeSet<Integer> v_set = new TreeSet<Integer>();
+        v_set.add(StdIn.readInt());
+        v_set.add(StdIn.readInt());
+        StdOut.println("Give 2 query vertices in the 2nd set:");        
+        TreeSet<Integer> w_set = new TreeSet<Integer>();
+        w_set.add(StdIn.readInt());
+        w_set.add(StdIn.readInt());
+        StdOut.println("Common Ancestor of " + v_set + " and " + w_set);
+        sap.bfs_(v_set, w_set);
+        StdOut.println(sap.ca_of_2_sets_);
+        // test ancestor(Iterable<Integer>, Iterable<Integer>)
+        StdOut.println("Common Ancestor of " + v_set + " and " + w_set);
+        StdOut.println(sap.ancestor(v_set, w_set));
+        // test length(Iterable<Integer>, Iterable<Integer>)
+        StdOut.println(
+            "Length of shortest ancestral path between " + 
+            v_set + " and " + w_set);
+        StdOut.println(sap.length(v_set, w_set));
     }
 }
