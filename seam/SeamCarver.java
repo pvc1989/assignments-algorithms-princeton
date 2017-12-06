d919bbff375eea9e3b0aca3b8828c5987f8a5606
Weicheng Pei <weicheng.pei@icloud.com>
2017-12-06 15:13:51 +0800

    implement constructor

--- seam/SeamCarver.java
+++ seam/SeamCarver.java
@@ -1,33 +1,56 @@
+/******************************************************************************
+ *  Compilation:    javac-algs4 SeamCarver.java
+ *  Execution:      java-algs4 SeamCarver.java filename.png
+ *
+ ******************************************************************************/
+
 import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.StdOut;
 
 public class SeamCarver {
+    private int[][] itsRGB;
 
     /**
      * Create a seam carver object based on the given picture.
      */
-    public SeamCarver(Picture picture) {
-
+    public SeamCarver(Picture pic) {
+        int h = pic.height();
+        int w = pic.width();
+        itsRGB = new int[h][w];
+        for (int r = 0; r != h; ++r) {
+            for (int c = 0; c != w; ++c) {
+                itsRGB[r][c] = pic.getRGB(c, r);
+            }
+        }
     }
     
     /**
      * current picture
      */
     public Picture picture() {
-        return null;
+        int h = height();
+        int w = width();
+        Picture pic = new Picture(w, h);
+        for (int r = 0; r != h; ++r) {
+            for (int c = 0; c != w; ++c) {
+                pic.setRGB(c, r, itsRGB[r][c]);
+            }
+        }
+        return pic;
     }
 
     /**
      * width of current picture
      */
     public int width() {
-        return 0;
+        return itsRGB[0].length;
     }
 
     /**
      * height of current picture
      */
     public int height() {
-        return 0;
+        return itsRGB.length;
     }
 
     /**
@@ -63,5 +86,17 @@ public class SeamCarver {
      */
     public void removeVerticalSeam(int[] seam) {
 
-    }  
- }
+    }
+
+    /**
+     * test client
+     */
+    public static void main(String[] args) {
+        // test the constructor and picture()
+        Picture pic = new Picture(args[0]);
+        pic.show();
+        SeamCarver sc = new SeamCarver(pic);
+        StdOut.printf("Size of the image: %dx%d\n", sc.width(), sc.height());
+        sc.picture().show();
+    }
+}
