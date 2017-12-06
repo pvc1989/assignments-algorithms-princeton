654f8b63952c97fea37cf192d697e68ae4502cc1
Weicheng Pei <weicheng.pei@icloud.com>
2017-12-06 20:04:15 +0800

    implement seam removing methods

--- seam/SeamCarver.java
+++ seam/SeamCarver.java
@@ -11,22 +11,24 @@ public class SeamCarver {
     private static final boolean DEBUG = false;
     private int[][] itsRGB;
     private double[][] itsEnergy;
+    private int itsHeight;
+    private int itsWidth;
 
     /**
      * Create a seam carver object based on the given picture.
      */
     public SeamCarver(Picture pic) {
-        int h = pic.height();
-        int w = pic.width();
-        itsRGB = new int[h][w];
-        for (int r = 0; r != h; ++r) {
-            for (int c = 0; c != w; ++c) {
+        itsHeight = pic.height();
+        itsWidth = pic.width();
+        itsRGB = new int[itsHeight][itsWidth];
+        for (int r = 0; r != itsHeight; ++r) {
+            for (int c = 0; c != itsWidth; ++c) {
                 itsRGB[r][c] = pic.getRGB(c, r);
             }
         }
-        itsEnergy = new double[h][w];
-        for (int r = 0; r != h; ++r) {
-            for (int c = 0; c != w; ++c) {
+        itsEnergy = new double[itsHeight][itsWidth];
+        for (int r = 0; r != itsHeight; ++r) {
+            for (int c = 0; c != itsWidth; ++c) {
                 itsEnergy[r][c] = energy(c, r);
             }
         }
@@ -36,11 +38,9 @@ public class SeamCarver {
      * @return current picture
      */
     public Picture picture() {
-        int h = height();
-        int w = width();
-        Picture pic = new Picture(w, h);
-        for (int r = 0; r != h; ++r) {
-            for (int c = 0; c != w; ++c) {
+        Picture pic = new Picture(itsWidth, itsHeight);
+        for (int r = 0; r != itsHeight; ++r) {
+            for (int c = 0; c != itsWidth; ++c) {
                 pic.setRGB(c, r, itsRGB[r][c]);
             }
         }
@@ -51,14 +51,14 @@ public class SeamCarver {
      * @return width of current picture
      */
     public int width() {
-        return itsRGB[0].length;
+        return itsWidth;
     }
 
     /**
      * @return height of current picture
      */
     public int height() {
-        return itsRGB.length;
+        return itsHeight;
     }
 
     /**
@@ -82,12 +82,18 @@ public class SeamCarver {
      * @return whether the pixel is on the border of the picture
      */
     private boolean onBorder(int c, int r) {
-        if (c == 0 || c == width() - 1 || r == 0 || r == height() - 1) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        if (c == 0 || c == itsWidth - 1) return true;
+        if (r == 0 || r == itsHeight - 1) return true;
+        return false;
+    }
+
+    /**
+     * @return whether the pixel is within the closed domain of the picture
+     */
+    private boolean inClosedDomain(int c, int r) {
+        if (c < 0 || c >= itsWidth) return false;
+        if (r < 0 || r >= itsHeight) return false;
+        return true;
     }
 
     /**
@@ -110,32 +116,30 @@ public class SeamCarver {
      * @return sequence of indices for horizontal seam
      */
     public int[] findHorizontalSeam() {
-        int h = height();
-        int w = width();
-        double[][] dist = new double[w][h];
-        int[][] prev = new int[w][h];
-        for (int c = 1; c != w; ++c) {
-            for (int r = 0; r != h; ++r) {
+        double[][] dist = new double[itsWidth][itsHeight];
+        int[][] prev = new int[itsWidth][itsHeight];
+        for (int c = 1; c != itsWidth; ++c) {
+            for (int r = 0; r != itsHeight; ++r) {
                 dist[c][r] = Double.MAX_VALUE;
             }
         }
-        for (int c = 0; c != w - 1; ++c) {
+        for (int c = 0; c != itsWidth - 1; ++c) {
             relaxCol(c, dist, prev);
         }
         // find the outlet of the path
         double minDist = Double.MAX_VALUE;
         int minRow = 0;
-        double[] distLastCol = dist[w-1];
-        for (int r = 0; r != h; ++r) {
+        double[] distLastCol = dist[itsWidth - 1];
+        for (int r = 0; r != itsHeight; ++r) {
             if (distLastCol[r] < minDist) {
                 minDist = distLastCol[r];
                 minRow = r;
             }
         }
         // build the path
-        int[] seam = new int[w];
-        seam[w-1] = minRow;
-        for (int c = w-1; c != 0; --c) {
+        int[] seam = new int[itsWidth];
+        seam[itsWidth - 1] = minRow;
+        for (int c = itsWidth - 1; c != 0; --c) {
             seam[c-1] = prev[c][seam[c]];
         }
         return seam;       
@@ -146,7 +150,7 @@ public class SeamCarver {
      */
     private void relaxCol(int c, double[][] dist, int[][] prev) {
         if (c >= width() - 1) return;
-        for (int r = 0; r != height(); ++r) {
+        for (int r = 0; r != itsHeight; ++r) {
             if (dist[c+1][r] > dist[c][r] + itsEnergy[r][c]) {
                 // relax the edge point to the East
                 dist[c+1][r] = dist[c][r] + itsEnergy[r][c];
@@ -173,30 +177,30 @@ public class SeamCarver {
     public int[] findVerticalSeam() {
         int h = height();
         int w = width();
-        double[][] dist = new double[h][w];
-        int[][] prev = new int[h][w];
-        for (int r = 1; r != h; ++r) {
-            for (int c = 0; c != w; ++c) {
+        double[][] dist = new double[itsHeight][itsWidth];
+        int[][] prev = new int[itsHeight][itsWidth];
+        for (int r = 1; r != itsHeight; ++r) {
+            for (int c = 0; c != itsWidth; ++c) {
                 dist[r][c] = Double.MAX_VALUE;
             }
         }
-        for (int r = 0; r != h - 1; ++r) {
+        for (int r = 0; r != itsHeight - 1; ++r) {
             relaxRow(r, dist, prev);
         }
         // find the outlet of the path
         double minDist = Double.MAX_VALUE;
         int minCol = 0;
-        double[] distLastRow = dist[h-1];
-        for (int c = 0; c != w; ++c) {
+        double[] distLastRow = dist[itsHeight - 1];
+        for (int c = 0; c != itsWidth; ++c) {
             if (distLastRow[c] < minDist) {
                 minDist = distLastRow[c];
                 minCol = c;
             }
         }
         // build the path
-        int[] seam = new int[h];
+        int[] seam = new int[itsHeight];
         seam[h-1] = minCol;
-        for (int r = h-1; r != 0; --r) {
+        for (int r = itsHeight - 1; r != 0; --r) {
             seam[r-1] = prev[r][seam[r]];
         }
         return seam;
@@ -207,7 +211,7 @@ public class SeamCarver {
      */
     private void relaxRow(int r, double[][] dist, int[][] prev) {
         if (r >= height() - 1) return;
-        for (int c = 0; c != width(); ++c) {
+        for (int c = 0; c != itsWidth; ++c) {
             if (dist[r+1][c] > dist[r][c] + itsEnergy[r][c]) {
                 // relax the edge point to the bottom
                 dist[r+1][c] = dist[r][c] + itsEnergy[r][c];
@@ -232,14 +236,42 @@ public class SeamCarver {
      * remove horizontal seam from current picture
      */
     public void removeHorizontalSeam(int[] seam) {
-
+        for (int c = 0; c != itsWidth; ++c) {
+            int r = seam[c];
+            // shift the rest of this column one step up
+            for (int k = r; k != itsHeight - 1; ++k) {
+                itsRGB[k][c] = itsRGB[k+1][c];
+                itsEnergy[k][c] = itsEnergy[k+1][c];
+            }
+        }
+        --itsHeight;
+        // update itsEnergy along the seam
+        for (int c = 0; c != itsWidth; ++c) {
+            int r = seam[c];
+            if (inClosedDomain(c, r)) itsEnergy[r][c] = energy(c, r);
+            if (inClosedDomain(c, r-1)) itsEnergy[r-1][c] = energy(c, r-1);
+        }
     }
 
     /**
      * remove vertical seam from current picture
      */
     public void removeVerticalSeam(int[] seam) {
-
+        for (int r = 0; r != itsHeight; ++r) {
+            int c = seam[r];
+            // shift the rest of this row one step left
+            for (int k = c; k != itsWidth - 1; ++k) {
+                itsRGB[r][k] = itsRGB[r][k+1];
+                itsEnergy[r][k] = itsEnergy[r][k+1];
+            }
+        }
+        --itsWidth;
+        // update itsEnergy along the seam
+        for (int r = 0; r != itsHeight; ++r) {
+            int c = seam[r];
+            if (inClosedDomain(c, r)) itsEnergy[r][c] = energy(c, r);
+            if (inClosedDomain(c-1, r)) itsEnergy[r][c-1] = energy(c-1, r);
+        }
     }
 
     /**
