240e128f95efe1be72b8689191e7ec58b2697cf5
Weicheng Pei <weicheng.pei@icloud.com>
2017-12-06 19:08:42 +0800

    implement seam identification methods

--- seam/SeamCarver.java
+++ seam/SeamCarver.java
@@ -10,6 +10,7 @@ import edu.princeton.cs.algs4.StdOut;
 public class SeamCarver {
     private static final boolean DEBUG = false;
     private int[][] itsRGB;
+    private double[][] itsEnergy;
 
     /**
      * Create a seam carver object based on the given picture.
@@ -21,6 +22,12 @@ public class SeamCarver {
         for (int r = 0; r != h; ++r) {
             for (int c = 0; c != w; ++c) {
                 itsRGB[r][c] = pic.getRGB(c, r);
+            }
+        }
+        itsEnergy = new double[h][w];
+        for (int r = 0; r != h; ++r) {
+            for (int c = 0; c != w; ++c) {
+                itsEnergy[r][c] = energy(c, r);
             }
         }
     }
@@ -86,7 +93,7 @@ public class SeamCarver {
     /**
      * @return squared central difference of rgb values
      */
-    private static double rgbGradSq(int rgb1, int rgb2) {
+    private static int rgbGradSq(int rgb1, int rgb2) {
         int r1 = (rgb1 >> 16) & 0xFF;
         int r2 = (rgb2 >> 16) & 0xFF;
         int dr = r1 - r2;
@@ -103,14 +110,122 @@ public class SeamCarver {
      * @return sequence of indices for horizontal seam
      */
     public int[] findHorizontalSeam() {
-        return null;
+        int h = height();
+        int w = width();
+        double[][] dist = new double[w][h];
+        int[][] prev = new int[w][h];
+        for (int c = 1; c != w; ++c) {
+            for (int r = 0; r != h; ++r) {
+                dist[c][r] = Double.MAX_VALUE;
+            }
+        }
+        for (int c = 0; c != w - 1; ++c) {
+            relaxCol(c, dist, prev);
+        }
+        // find the outlet of the path
+        double minDist = Double.MAX_VALUE;
+        int minRow = 0;
+        double[] distLastCol = dist[w-1];
+        for (int r = 0; r != h; ++r) {
+            if (distLastCol[r] < minDist) {
+                minDist = distLastCol[r];
+                minRow = r;
+            }
+        }
+        // build the path
+        int[] seam = new int[w];
+        seam[w-1] = minRow;
+        for (int c = w-1; c != 0; --c) {
+            seam[c-1] = prev[c][seam[c]];
+        }
+        return seam;       
+    }
+
+    /**
+     * relax all the vertices in the c-th column.
+     */
+    private void relaxCol(int c, double[][] dist, int[][] prev) {
+        if (c >= width() - 1) return;
+        for (int r = 0; r != height(); ++r) {
+            if (dist[c+1][r] > dist[c][r] + itsEnergy[r][c]) {
+                // relax the edge point to the East
+                dist[c+1][r] = dist[c][r] + itsEnergy[r][c];
+                prev[c+1][r] = r;
+            }
+            if (r != 0 &&
+                dist[c+1][r-1] > dist[c][r] + itsEnergy[r][c]) {
+                // relax the edge point to the NorthEast
+                dist[c+1][r-1] = dist[c][r] + itsEnergy[r][c];
+                prev[c+1][r-1] = r;
+            }
+            if (r != height() - 1 &&
+                dist[c+1][r+1] > dist[c][r] + itsEnergy[r][c]) {
+                // relax the edge point to the SouthEast
+                dist[c+1][r+1] = dist[c][r] + itsEnergy[r][c];
+                prev[c+1][r+1] = r;
+            }
+        }
     }
 
     /**
      * @return sequence of indices for vertical seam
      */
     public int[] findVerticalSeam() {
-        return null;
+        int h = height();
+        int w = width();
+        double[][] dist = new double[h][w];
+        int[][] prev = new int[h][w];
+        for (int r = 1; r != h; ++r) {
+            for (int c = 0; c != w; ++c) {
+                dist[r][c] = Double.MAX_VALUE;
+            }
+        }
+        for (int r = 0; r != h - 1; ++r) {
+            relaxRow(r, dist, prev);
+        }
+        // find the outlet of the path
+        double minDist = Double.MAX_VALUE;
+        int minCol = 0;
+        double[] distLastRow = dist[h-1];
+        for (int c = 0; c != w; ++c) {
+            if (distLastRow[c] < minDist) {
+                minDist = distLastRow[c];
+                minCol = c;
+            }
+        }
+        // build the path
+        int[] seam = new int[h];
+        seam[h-1] = minCol;
+        for (int r = h-1; r != 0; --r) {
+            seam[r-1] = prev[r][seam[r]];
+        }
+        return seam;
+    }
+    
+    /**
+     * relax all the vertices in the r-th row.
+     */
+    private void relaxRow(int r, double[][] dist, int[][] prev) {
+        if (r >= height() - 1) return;
+        for (int c = 0; c != width(); ++c) {
+            if (dist[r+1][c] > dist[r][c] + itsEnergy[r][c]) {
+                // relax the edge point to the bottom
+                dist[r+1][c] = dist[r][c] + itsEnergy[r][c];
+                prev[r+1][c] = c;
+            }
+            if (c != 0 &&
+                dist[r+1][c-1] > dist[r][c] + itsEnergy[r][c]) {
+                // relax the edge point to the bottom left
+                dist[r+1][c-1] = dist[r][c] + itsEnergy[r][c];
+                prev[r+1][c-1] = c;
+            }
+            if (c != width() - 1 &&
+                dist[r+1][c+1] > dist[r][c] + itsEnergy[r][c]) {
+                // relax the edge point to the bottom right
+                dist[r+1][c+1] = dist[r][c] + itsEnergy[r][c];
+                prev[r+1][c+1] = c;
+            }
+        }
     }
 
     /**
